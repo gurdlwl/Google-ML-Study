73c996621434b6401679b1f46050d73bb48e72f1
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-03-29 20:38:36 +0900

    Fourth commit

---
+++ pipeline.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .5)
+
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
+
+my_classifier.fit(X_train, Y_train)
+
+predictions = my_classifier.predict(X_test)
+print(predictions)
