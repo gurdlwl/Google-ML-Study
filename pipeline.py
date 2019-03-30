4db0298e1d1157ec6b523c931ac607ef75384f26
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-03-30 16:25:27 +0900

    Fourth commit

--- pipeline.py
+++ pipeline.py
@@ -4,13 +4,22 @@ iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
-from sklearn.cross_validation import train_test_split
+#cross_validation 이 오류가 난다면 model_selection을 사용하자
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .5)
 
-from sklearn import tree
-my_classifier = tree.DecisionTreeClassifier()
+#from sklearn import tree
+#my_classifier = tree.DecisionTreeClassifier()
+
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
 
 my_classifier.fit(X_train, Y_train)
 
 predictions = my_classifier.predict(X_test)
 print(predictions)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test, predictions))
+ 
+
