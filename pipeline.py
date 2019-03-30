9aee7deeafc40f1fbb90276d79201c3deea13035
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-03-30 16:29:59 +0900

    Fourth commit

--- pipeline.py
+++ pipeline.py
@@ -4,12 +4,12 @@ iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
-#cross_validation 이 오류가 난다면 model_selection을 사용하자
+# cross_validation 이 오류가 난다면 model_selection을 사용하자
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .5)
 
-#from sklearn import tree
-#my_classifier = tree.DecisionTreeClassifier()
+# from sklearn import tree
+# my_classifier = tree.DecisionTreeClassifier()
 
 from sklearn.neighbors import KNeighborsClassifier
 my_classifier = KNeighborsClassifier()
@@ -22,4 +22,5 @@ print(predictions)
 from sklearn.metrics import accuracy_score
 print(accuracy_score(Y_test, predictions))
  
-
+# playground.tensorflow.org
+# 신경망관련 웹사이트. 참고
